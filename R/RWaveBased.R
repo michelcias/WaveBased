b33db1595a956f1424cecea8996407d5aed048e4
michelcias <michelcias@gmail.com>
2022-05-05 15:33:05 -0300

    New changes:
    * Adapted the codes to accept a wavelet filter provided by the user
    
    * Updated stop and warning messeges

--- R/RWaveBased.R
+++ R/RWaveBased.R
@@ -1,156 +1,116 @@
-#########################################################################################
+################################################################################
 
 PHI <- function(x, J, family = "Daublets", filter.size = 20, prec.wavelet = 30,
-                periodic = TRUE){
+                periodic = TRUE, wavelet.filter){
   
   if(is.complex(x)){
     x <- Re(x)
-    warning("Sorry! We don't work with complex data. Only the real part will be
-            considered.")
+    warning("Sorry, we don't work with complex data. Only the real part was considered.")
   }
   
-  fam <- which(tolower(substring(family, 1, 1)) == c("d", "s", "c"))
+  fam <- which(tolower(substring(family, 1, 1)) == c("d", "s", "c", "o"))
   
   if(length(fam) == 0)
-    stop("Unknown family. The families available are 'Daublets', 'Symmlets' and 'Coiflets'.")
+    stop("Unknown family. The families available are 'Daublets', 'Symmlets' and 'Coiflets'. You can also use 'Own', if you provide your wavelet.filter.")
   
-  PHI <- .Call("C_PHImat", x, as.integer(J), as.integer(fam), as.integer(filter.size),
-               as.integer(prec.wavelet), as.integer(periodic))
+  if(missing(wavelet.filter)){
+    if(fam == 4)
+      stop("Provide your own filter or choose an available family.")
+    else
+      wavelet.filter <- 0
+  }
+  else{
+    if(any(fam == 1:3))
+      warning("wavelet.filter provided, but family is not 'Own'. The arguments family and filter.size were ignored.")
+    fam <- 4
+  }
+  
+  PHI <- .Call("C_PHImat", as.double(x), as.integer(J), as.integer(fam),
+               as.integer(filter.size), as.integer(prec.wavelet),
+               as.integer(periodic), as.double(wavelet.filter))
   return(PHI)
   
 }
-#########################################################################################
+################################################################################
 
 PSI <- function(x, J, family = "Daublets", filter.size = 20, prec.wavelet = 30,
-                 periodic = TRUE){
+                 periodic = TRUE, wavelet.filter){
   
   if(is.complex(x)){
     x <- Re(x)
-    warning("Sorry! We don't work with complex data. Only the real part will be
-            considered.")
+    warning("Sorry, we don't work with complex data. Only the real part was considered.")
   }
   
-  fam <- which(tolower(substring(family, 1, 1)) == c("d", "s", "c"))
+  fam <- which(tolower(substring(family, 1, 1)) == c("d", "s", "c", "o"))
   
   if(length(fam) == 0)
-    stop("Unknown family. The families available are 'Daublets', 'Symmlets' and 'Coiflets'.")
+    stop("Unknown family. The families available are 'Daublets', 'Symmlets' and 'Coiflets'. You can also use 'Own', if you provide your wavelet.filter.")
   
-  PSI <- .Call("C_PSImat", x, as.integer(J), as.integer(fam), as.integer(filter.size),
-               as.integer(prec.wavelet), as.integer(periodic))
+  if(missing(wavelet.filter)){
+    if(fam == 4)
+      stop("Provide your own filter or choose an available family.")
+    else
+      wavelet.filter <- 0
+  }
+  else{
+    if(any(fam == 1:3))
+      warning("wavelet.filter provided, but family is not 'Own'. The arguments family and filter.size were ignored.")
+    fam <- 4
+  }
+  
+  PSI <- .Call("C_PSImat", as.double(x), as.integer(J), as.integer(fam),
+               as.integer(filter.size), as.integer(prec.wavelet),
+               as.integer(periodic), as.double(wavelet.filter))
   
   return(PSI)
   
 }
-#########################################################################################
+################################################################################
 
-# wavelet.basis <- function(x, tau = 1, j0 = 0, J, filter.number = 10, family = "DaubLeAsymm",
-# prec.wavelet = 30, periodic = TRUE){
-#     
-#     if(j0 > J) stop("The coarsest level j0 cannot be greater than the finest level J!")
-#     
-#     if((tau != 1) & (periodic)){
-#         tau <- 1
-#         warning("The periodic version was calculated with tau = 1.")
-#     }
-#     
-#     if(j0 == J){
-#         if(periodic)
-#         cnames <- paste("phi[", j0, ",", 0:(tau * 2^j0 - 1), "]", sep = "")
-#         
-#         else{
-#             sup <- support(filter.number, family)
-#             phi.lh <- sup$phi.lh
-#             phi.rh <- sup$phi.rh
-#             kminphi <- c(ceiling(tau * 2^j0 * min(x) - phi.rh + 1e-9))
-#             kmaxphi <- c(floor(tau * 2^j0 * max(x) - phi.lh))
-#             cnames <- paste("phi[", tau * 2^j0, ",", kminphi:kmaxphi, "]", sep = "")
-#         }
-#         
-#         mat <- PHI(x, tau, j0, filter.number, family, prec.wavelet, periodic)
-#         colnames(mat) <- cnames
-#         return(mat)
-#     }
-#     
-#     n <- length(x)
-#     p <- tau * 2^(j0:(J-1))
-#     
-#     if(periodic){
-#         ncolphi <- p[1]
-#         kminphi <- 0
-#         kmaxphi <- ncolphi - 1
-#         
-#         ncolpsi <- p
-#         kminpsi <- rep(0, length.out = J-j0)
-#         kmaxpsi <- ncolpsi - 1
-#     }
-#     
-#     else{
-#         sup <- support(filter.number, family)
-#         phi.lh <- sup$phi.lh
-#         phi.rh <- sup$phi.rh
-#         psi.lh <- sup$psi.lh
-#         psi.rh <- sup$psi.rh
-#         
-#         kminphi <- c(ceiling(p[1] * min(x) - phi.rh + 1e-9))
-#         kmaxphi <- c(floor(p[1] * max(x) - phi.lh))
-#         ncolphi <- kmaxphi - kminphi + 1
-#         
-#         kminpsi <- c(ceiling(p * min(x) - psi.rh + 1e-9))
-#         kmaxpsi <- c(floor(p * max(x) - psi.lh))
-#         ncolpsi <- kmaxpsi - kminpsi + 1
-#     }
-#     
-#     ncolmat <- ncolphi + sum(ncolpsi)
-#     
-#     mat <- matrix(nrow = n, ncol = ncolmat)
-#     cnames <- 1:ncolmat
-#     
-#     mat[, 1:ncolphi] <- PHI(x, tau, j0, filter.number, family, prec.wavelet, periodic)
-#     cnames[1:ncolphi] <- paste("phi[", j0, ",", kminphi:kmaxphi, "]", sep = "")
-#     idx <- ncolphi
-#     
-#     for(j in j0:(J-1)){
-#         mat[, idx + 1:ncolpsi[j-j0+1]] <- PSI(x, tau, j, filter.number, family,
-#                                               prec.wavelet, periodic)
-#         cnames[idx + 1:ncolpsi[j-j0+1]] <- paste("psi[", j, ",", kminpsi[j-j0+1]:kmaxpsi[j-j0+1], "]", sep = "")
-#         idx <- idx + ncolpsi[j-j0+1]
-#     }
-#     
-#     colnames(mat) <- cnames
-#     return(mat)
-# }
-#########################################################################################
-
-wbasis <- function(x, j0 = 0, J, family = "Daublets", filter.size = 20, prec.wavelet = 30,
-                   periodic = TRUE){
+wbasis <- function(x, j0 = 0, J, family = "Daublets", filter.size = 20,
+                   prec.wavelet = 30, periodic = TRUE, wavelet.filter){
   
   if(is.complex(x)){
     x <- Re(x)
-    warning("Sorry! We don't work with complex data. Only the real part will be
-            considered.")
+    warning("Sorry, we don't work with complex data. Only the real part was considered.")
   }
   
   if(j0 < 0)
-    stop("'j0' must be an integer greater than or equal to zero.")
+    stop("j0 must be a non-negative integer.")
   
-  fam <- which(tolower(substring(family, 1, 1)) == c("d", "s", "c"))
+  fam <- which(tolower(substring(family, 1, 1)) == c("d", "s", "c", "o"))
   
   if(length(fam) == 0)
-    stop("Unknown family. The families available are 'Daublets', 'Symmlets' and 'Coiflets'.")
+    stop("Unknown family. The families available are 'Daublets', 'Symmlets' and 'Coiflets'. You can also use 'Own', if you provide your wavelet.filter.")
   
-  wmat <- .Call("C_WavBasis", x, as.integer(j0), as.integer(J), as.integer(fam), as.integer(filter.size),
-               as.integer(prec.wavelet), as.integer(periodic))
+  if(missing(wavelet.filter)){
+    if(fam == 4)
+      stop("Provide your own filter or choose an available family.")
+    else
+      wavelet.filter <- 0
+  }
+  else{
+    if(any(fam == 1:3))
+      warning("wavelet.filter provided, but family is not 'Own'. The arguments family and filter.size were ignored.")
+    fam <- 4
+  }
+  
+  wmat <- .Call("C_WavBasis", as.double(x), as.integer(j0), as.integer(J),
+                as.integer(fam), as.integer(filter.size),
+                as.integer(prec.wavelet), as.integer(periodic),
+                as.double(wavelet.filter))
   
   return(wmat)
   
 }
-#########################################################################################
+################################################################################
 
 wdensity <- function(data, from, to, length.obs = 250, wf = NULL, power.dens,
-                     J0 = 0, J1, family = "Daublets", filter.size = 10, prec.wavelet = 30,
-                     dens.biased, warped = TRUE, warp.fun, dwarp.fun, rescale = TRUE,
-                     eps = 1.9^(-J1), thresh = "hard", plot = TRUE, main, xlab, ylab,
-                     type, ...){
+                     J0 = 0, J1, family = "Daublets", filter.size = 10,
+                     prec.wavelet = 30, wavelet.filter, dens.biased, 
+                     warped = TRUE, warp.fun, dwarp.fun, rescale = TRUE,
+                     eps = 1.9^(-J1), thresh = "hard", plot = TRUE, main, xlab,
+                     ylab, type, ...){
   
   if (!any(is.finite(data))){
     data <- data[is.finite(data)]
@@ -162,12 +122,12 @@ wdensity <- function(data, from, to, length.obs = 250, wf = NULL, power.dens,
   
   thresh <- tolower(substring(thresh, 1, 1))
   if(!thresh %in% c("l","h","s"))
-    stop("The options available for 'thresh' are 'linear', 'hard' and 'soft'.")
+    stop("The options available for thresh are 'linear', 'hard' and 'soft'.")
   
   if(missing(power.dens))
     power.dens <- 0.5
   if(power.dens < 0.5)
-    stop("'power.dens' must be greater than or equal to 0.5.")
+    stop("power.dens must be greater than or equal to 0.5.")
   
   ry <- range(data)
   sy <- sort(data)
@@ -177,10 +137,7 @@ wdensity <- function(data, from, to, length.obs = 250, wf = NULL, power.dens,
     from <- sy[1]
   if(missing(to))
     to <- sy[n]
-  if(from < sy[1])
-    stop("'from' must be greater than or equal to min(data).")
-  if(to > sy[n])
-    stop("'to' must be less than or equal to max(data).")
+  
   if(from > to)
     stop("'from' must be smaller than or equal to 'to'.")
   if(missing(dens.biased)){
@@ -198,11 +155,11 @@ wdensity <- function(data, from, to, length.obs = 250, wf = NULL, power.dens,
       Hy.vals <- approx(x = sy, y = Hyc, xout = y.vals)$y
     }
     else if(!is.function(warp.fun))
-      stop("'warp.fun' must be a function.")
+      stop("warp.fun must be a function.")
     else if(missing(dwarp.fun))
-      stop("'dwarp.fun' must be a function.")
+      stop("dwarp.fun must be a function.")
     else if(!is.function(dwarp.fun))
-      stop("'dwarp.fun' must be a function.")
+      stop("dwarp.fun must be a function.")
     else{
       Hyc <- warp.fun(sy)
       hy <- dwarp.fun(sy)
@@ -224,10 +181,12 @@ wdensity <- function(data, from, to, length.obs = 250, wf = NULL, power.dens,
     Hy.vals <- y.vals
   }
   
-  mbasis <- PHI(x = c(Hyc, Hy.vals), J = J1, family = family, filter.size = filter.size,
-                prec.wavelet = prec.wavelet, periodic = TRUE)
+  mbasis <- PHI(x = c(Hyc, Hy.vals), J = J1, family = family,
+                filter.size = filter.size, prec.wavelet = prec.wavelet,
+                periodic = TRUE, wavelet.filter = wavelet.filter)
   
-  coefs <- (scale/mean(1/wf(sy)))^(power.dens)*colMeans(mbasis[seq_len(length.out = n),]*dens.biased^(power.dens-1)*hy/wf(sy)^power.dens)
+  matvals <- mbasis[seq_len(n),]*dens.biased^(power.dens-1)*hy/wf(sy)^power.dens
+  coefs <- (scale/mean(1/wf(sy)))^(power.dens)*colMeans(matvals)
   
   if(thresh == "h"){
     coefs <- wavedec(x = coefs, j0 = J0, family = family,
@@ -253,8 +212,8 @@ wdensity <- function(data, from, to, length.obs = 250, wf = NULL, power.dens,
   if(!warped & power.dens == 0.5)
     coefs <- coefs/sqrt(sum(coefs^2))
   
-  dens.vals <- drop(mbasis[-seq_len(length.out = n),]%*%coefs)^(1/power.dens)/scale
-  
+  dens.vals <- drop(mbasis[-seq_len(n),]%*%coefs)^(1/power.dens)/scale
+
   if((1/power.dens) %% 2)
     dens.vals[dens.vals < 0] <- 0
   
@@ -275,43 +234,82 @@ wdensity <- function(data, from, to, length.obs = 250, wf = NULL, power.dens,
   return(invisible(list(y = y.vals, dens = dens.vals)))
   
 }
-#########################################################################################
+################################################################################
 
-wavedec <- function(x, j0 = 0, family = "Daublets", filter.size = 20){
+wavedec <- function(x, j0 = 0, family = "Daublets", filter.size = 20,
+                    wavelet.filter){
   
-  if(j0 < 0 | j0 != trunc(j0))
-    stop("'j0' must be an integer greater than or equal to zero.")
-  
-  fam <- which(tolower(substring(family, 1, 1)) == c("d", "s", "c"))
-  
-  if(length(fam) == 0)
-    stop("Unknown family. The families available are 'Daublets',
-         'Symmlets' and 'Coiflets'.")
+  if(is.complex(x)){
+    x <- Re(x)
+    warning("Sorry, we don't work with complex data. Only the real part was considered.")
+  }
   
   if(any(!is.finite(x)))
     stop("'x' must be a numeric vector.")
   
-  wdec <- .Call("C_WaveDec", as.numeric(x), as.integer(fam), as.integer(filter.size),
-                as.integer(j0))
+  if(j0 < 0)
+    stop("j0 must be a non-negative integer.")
+  
+  fam <- which(tolower(substring(family, 1, 1)) == c("d", "s", "c", "o"))
+  
+  if(length(fam) == 0)
+    stop("Unknown family. The families available are 'Daublets', 'Symmlets' and 'Coiflets'. You can also use 'Own', if you provide your wavelet.filter.")
+  
+  if(missing(wavelet.filter)){
+    if(fam == 4)
+      stop("Provide your own filter or choose an available family.")
+    else
+      wavelet.filter <- 0
+  }
+  else{
+    if(any(fam == 1:3))
+      warning("wavelet.filter provided, but family is not 'Own'. The arguments family and filter.size were ignored.")
+    fam <- 4
+  }
+  
+  wdec <- .Call("C_WaveDec", as.double(x), as.integer(fam),
+                as.integer(filter.size), as.integer(j0),
+                as.double(wavelet.filter))
   
   return(wdec)
   
 }
-#########################################################################################
+################################################################################
 
-waverec <- function(x, j0 = 0, family = "Daublets", filter.size = 20){
+waverec <- function(x, j0 = 0, family = "Daublets", filter.size = 20,
+                    wavelet.filter){
   
-  if(j0 < 0 | j0 != trunc(j0))
-    stop("'j0' must be an integer greater than or equal to zero.")
+  if(is.complex(x)){
+    x <- Re(x)
+    warning("Sorry, we don't work with complex data. Only the real part was considered.")
+  }
   
-  fam <- which(tolower(substring(family, 1, 1)) == c("d", "s", "c"))
+  if(any(!is.finite(x)))
+    stop("'x' must be a numeric vector.")
+  
+  if(j0 < 0)
+    stop("j0 must be a non-negative integer.")
+  
+  fam <- which(tolower(substring(family, 1, 1)) == c("d", "s", "c", "o"))
   
   if(length(fam) == 0)
-    stop("Unknown family. The families available are 'Daublets',
-         'Symmlets' and 'Coiflets'.")
+    stop("Unknown family. The families available are 'Daublets', 'Symmlets' and 'Coiflets'. You can also use 'Own', if you provide your wavelet.filter.")
   
-  wrec <- .Call("C_WaveRec", as.numeric(x), as.integer(fam), as.integer(filter.size),
-                as.integer(j0))
+  if(missing(wavelet.filter)){
+    if(fam == 4)
+      stop("Provide your own filter or choose an available family.")
+    else
+      wavelet.filter <- 0
+  }
+  else{
+    if(any(fam == 1:3))
+      warning("wavelet.filter provided, but family is not 'Own'. The arguments family and filter.size were ignored.")
+    fam <- 4
+  }
+  
+  wrec <- .Call("C_WaveRec", as.double(x), as.integer(fam),
+                as.integer(filter.size), as.integer(j0),
+                as.double(wavelet.filter))
   
   return(wrec)
   
